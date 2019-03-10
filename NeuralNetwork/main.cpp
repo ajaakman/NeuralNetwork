4da2a66cfa77536ab5607f690c075184a0895b58
Artur Jaakman <jaakmanartur@gmail.com>
2019-03-10 11:57:51 +0200

    Rock, Paper, Scissors.

--- NeuralNetwork/main.cpp
+++ NeuralNetwork/main.cpp
@@ -4,11 +4,17 @@
 #include "TrainingData.h"
 #include "NeuralNetwork.h"
 
+#define RPS
+
 int main()
 {
+#if defined(XOR)
 	TrainingData trainingData("XORTraining.txt");
-
-	NeuralNetwork neuralNet(std::vector<unsigned>{2, 3, 1}, 0.15f, 0.5f, 100);
+	NeuralNetwork neuralNet(std::vector<unsigned>{2, 2, 1}, 0.15f, 0.5f, 100);
+#elif defined(RPS)
+	TrainingData trainingData("RPSTraining.txt");
+	NeuralNetwork neuralNet(std::vector<unsigned>{1, 3, 1}, 0.15f, 0.5f, 100);
+#endif
 	
 	for (auto & layer : neuralNet.GetNeuronData())
 	{
@@ -75,17 +81,34 @@ int main()
 
 	while (1)
 	{
+#if defined(XOR)
 		std::cout << "\nPlease insert First Value: ";
 		std::cin >> inputs[0];
 		std::cout << "Please insert Second Value: ";
 		std::cin >> inputs[1];
-
 		if (!neuralNet.ForwardPropagate(inputs))
 			break;
 
 		std::cout << "Neural Net Outputs:" << " ";
 		for (auto & output : neuralNet.GetOutputs())
 			std::cout << (int)round(output) << " ";
+#elif defined(RPS)
+		std::cout << "\nType r for rock, p for paper, s for scissors: ";
+		char in; std::cin >> in;		
+		if (in == 'r') inputs[0] = -1.f;
+		else if (in == 'p') inputs[0] = 0.f;
+		else if (in == 's') inputs[0] = 1.f;
+		else { std::cout << "\nInvalid input try again"; continue; }
+
+		if (!neuralNet.ForwardPropagate(inputs))
+			break;
+
+		std::cout << "Neural Net Outputs:" << " ";
+		if ((int)(round(neuralNet.GetOutputs()[0])) == -1) std::cout << "Rock";
+		if ((int)(round(neuralNet.GetOutputs()[0])) == -0) std::cout << "Paper";
+		if ((int)(round(neuralNet.GetOutputs()[0])) ==  1) std::cout << "Scissors";
+#endif
 	}
+	std::cin.get();
 }
 
